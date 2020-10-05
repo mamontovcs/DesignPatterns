8b2fed0ada6e964ce1313905981b9d4f935795cd
Vitaliy Mamontov <vintixfish@gmail.com>
2020-10-05 19:58:01 +0300

    Flyweight

---
+++ Flyweight/Models/Circle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flyweight.Models
+{
+    class Circle : IShape
+    {
+        private string _color;
+
+        public Circle(string color)
+        {
+            _color = color;
+        }
+
+        public string GetColor()
+        {
+            return _color;
+        }
+
+        public void Draw()
+        {
+            Console.WriteLine($"{_color} circle");
+        }
+    }
+}
---
+++ Flyweight/ShapeFactory.cs
@@ -0,0 +1,27 @@
+﻿using Flyweight.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flyweight
+{
+    class ShapeFactory
+    {
+        private static List<Circle> _circles = new List<Circle>() /*{ new Circle("Black")}*/;
+
+        public static IShape getCircle(string color)
+        {
+            Circle circle = _circles.Find(x => x.GetColor() == color);
+
+            if (circle == null)
+            {
+                circle = new Circle(color);
+                _circles.Add(circle);
+                Console.WriteLine("Creating circle of color : " + color);
+            }
+            return circle;
+        }
+    }
+}
